ff372ca0379372f141c63b19b4e1004f933af33f
Anartz Mugika <mugan86@gmail.com>
2016-04-14 10:17:46 +0200

    Register Track Screen View in Google Analytics

--- app/src/main/java/amuxika/googleanalytics/MainActivity.java
+++ app/src/main/java/amuxika/googleanalytics/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        GoogleAnalyticsApplication.getInstance().trackScreenView("Test");
+        GoogleAnalyticsApplication.getInstance().trackScreenView("Test Android Google Analytics");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
